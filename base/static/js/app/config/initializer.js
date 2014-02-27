df2f48f538be30e5f0a955c30db46be80f0cd239
Chris Cabral <chris.cabral@newbrandanalytics.com>
2014-02-27 01:11:32 -0500

    angular state addition

--- base/static/js/app/config/initializer.js
+++ base/static/js/app/config/initializer.js
@@ -9,17 +9,17 @@ var mainapp = angular.module('mainapp', ['ui.router', 'ui.bootstrap'])
     .state('state1', {
       	url: "/state1",
       	views:{
-      		"viewHeader": { template: "/static/js/app/templates/header.html" },
-      		"viewBody": { template: "/static/js/app/templates/state1.html" },
-      		"viewFooter": { template: "/static/js/app/templates/footer.html" }
+      		"viewHeader": { templateUrl: "/static/js/app/templates/header.html" },
+      		"viewBody": { templateUrl: "/static/js/app/templates/state1.html" },
+      		"viewFooter": { templateUrl: "/static/js/app/templates/footer.html" }
 		}
     })
     .state('state2', {
 		url: "/state2",
       	views:{
-      		"viewHeader": { template: "/static/js/app/templates/header.html" },
-      		"viewBody": { template: "/static/js/app/templates/state2.html" },
-      		"viewFooter": { template: "/static/js/app/templates/footer.html" }
+      		"viewHeader": { templateUrl: "/static/js/app/templates/header.html" },
+      		"viewBody": { templateUrl: "/static/js/app/templates/state2.html" },
+      		"viewFooter": { templateUrl: "/static/js/app/templates/footer.html" }
 		}
     });
     alert("app configuration ended")
