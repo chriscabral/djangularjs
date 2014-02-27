5a573230e80ca3b7deb323fdc99cbf0add64439e
Chris Cabral <chris.cabral@newbrandanalytics.com>
2014-02-27 00:56:48 -0500

    adding proper dependency injection

--- base/static/js/app/config/initializer.js
+++ base/static/js/app/config/initializer.js
@@ -1,10 +1,28 @@
 alert("app starting");
 
-var mainapp = angular.module('mainapp', [])
-.config(function(){
+var mainapp = angular.module('mainapp', ['ui.router', 'ui.bootstrap'])
+.config(function($stateProvider, $urlRouterProvider){
 	alert("app configuring");
-
-  alert("app configuration ended");
+	$urlRouterProvider.otherwise("/state1");
+	
+	$stateProvider
+    .state('state1', {
+      	url: "/state1",
+      	views:{
+      		"viewHeader": { template: "/static/js/app/templates/header.html" },
+      		"viewBody": { template: "/static/js/app/templates/state1.html" },
+      		"viewFooter": { template: "/static/js/app/templates/footer.html" }
+		}
+    })
+    .state('state2', {
+		url: "/state2",
+      	views:{
+      		"viewHeader": { template: "/static/js/app/templates/header.html" },
+      		"viewBody": { template: "/static/js/app/templates/state2.html" },
+      		"viewFooter": { template: "/static/js/app/templates/footer.html" }
+		}
+    });
+    alert("app configuration ended")
 });
 
 alert("app ending");
