05745472a60f13aaf36c7336abf464d447c74eb1
Chris Cabral <chris.cabral@newbrandanalytics.com>
2014-02-26 21:16:40 -0500

    initial commit

---
+++ project_name/settings.py
@@ -0,0 +1,162 @@
+import os
+import logging
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__) + "../../")
+SUPPORTED_NONLOCALES = ['media', 'admin', 'static']
+LANGUAGE_CODE = 'en-us'
+ROOT_URLCONF = '{{ project_name }}.urls'
+
+INSTALLED_APPS = (
+    # Django contrib apps
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.humanize',
+    'django.contrib.syndication',
+    'django.contrib.staticfiles',
+
+    # Third-party apps, patches, fixes
+    'djcelery',
+    'debug_toolbar',
+    'compressor',
+    'django_nose',
+    'south',
+    'base',
+)
+
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
+
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = False
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ()
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+TIME_ZONE = 'America/Los_Angeles'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.static',
+    'django.core.context_processors.csrf',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+]
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'templates'),
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+def custom_show_toolbar(request):
+    """ Only show the debug toolbar to users with the superuser flag. """
+    return request.user.is_superuser
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': '{{ project_name }}.settings.base.custom_show_toolbar',
+    'HIDE_DJANGO_SQL': True,
+    'TAG': 'body',
+    'SHOW_TEMPLATE_CONTEXT': True,
+    'ENABLE_STACKTRACES': True,
+}
+
+FILE_UPLOAD_PERMISSIONS = 0664
+WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db/development.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+
+    },
+}
+
+ADMINS = ()
+MANAGERS = ADMINS
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+DEBUG = TEMPLATE_DEBUG = bool(os.environ.get('DEBUG', False))
+DEV = True
+
+ALLOWED_HOSTS = []
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
+
+INTERNAL_IPS = ('127.0.0.1')
+
+# Uncomment these to activate and customize Celery:
+# CELERY_ALWAYS_EAGER = False  # required to activate celeryd
+# BROKER_HOST = 'localhost'
+# BROKER_PORT = 5672
+# BROKER_USER = 'django'
+# BROKER_PASSWORD = 'django'
+# BROKER_VHOST = 'django'
+# CELERY_RESULT_BACKEND = 'amqp'
+
+SERVER_EMAIL = "webmaster@example.com"
+DEFAULT_FROM_EMAIL = "webmaster@example.com"
+SYSTEM_EMAIL_PREFIX = "[{{ project_name }}]"
+
+LOG_LEVEL = logging.INFO
+HAS_SYSLOG = True
+SYSLOG_TAG = "http_app_{{ project_name }}"  # Make this unique to your project.
+LOGGING_CONFIG = None
+LOGGING = {
+    'version': 1,
+    'loggers': {
+        '{{ project_name }}': {
+            'level': "DEBUG"
+        }
+    }
+}
